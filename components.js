a6f06022ce3204adcbf586a26424dad3616ad2fb
Damien <damien.boisseau@gmail.com>
2016-06-15 17:05:36 +0200

    Ajout de la copie vers le presse papier

--- components.js
+++ components.js
@@ -18,19 +18,27 @@ var VoyellastorizerBox = React.createClass({
     this.setState({value: output});
   },
 
+  handleClick: function() {
+    let copyThis = document.getElementById('copyThis');
+    copyThis.select();
+    let successful = document.execCommand('copy');
+    console.log(successful);
+  },
+
   voyellastorizeThis: function() {
     return { __html: this.state.value };
   },
 
   render: function() {
     return (
-      <div className="voyellastorizer">
-        <h1>Le Voyellastorizer !</h1>
-        <h3>Input</h3>
-        <textarea onChange={this.handleChange} ref="textarea" defaultValue={this.state.value} />
-        <h3>Output</h3>
-        <div className="content" dangerouslySetInnerHTML={this.voyellastorizeThis()} />
-      </div>
+    <div className="voyellastorizer">
+      <h1>Le Voyellastorizer</h1>
+      <h3>Input</h3>
+      <textarea onChange={this.handleChange} ref="textarea" defaultValue={this.state.value} />
+      <h3>Output</h3>
+      <input className="content" id="copyThis" type="text" value={this.state.value} />
+      <button className="btn btn-default" onClick={this.handleClick}><span className="glyphicon glyphicon-duplicate"></span> Copier</button>
+    </div>
     );
   }
 });
